74fc9c79849da1d74d8de6df9b1860142af49c94
Trey Carey <treyacarey@icloud.com>
2021-11-23 00:11:13 -0600

    Add basic buttons and text

--- Main.py
+++ Main.py
@@ -10,24 +10,30 @@ programName = "New Window"
 #Creates the TK() Object
 master = Tk()
 
-#Sets the Geometry of main
-#Root window
-master.geometry("200x200")
-
+#Sets the Geometry & Title of main root window
+master.geometry("1024x768")
 master.title(programName)
 
-#Function to open a new window
-#On a button click
-def openNewWindow():
+welcomeLabel = Label(master, text = "Welcome to " + programName + "!")
+welcomeLabel.pack()
 
-    #TopLevel Object which will be treated as a new window
-    newWindow = Toplevel(master)
+optionsText = Label(master, text = "Please choose one of the following options")
+optionsText.pack()
 
-    #Sets the title of the TopLevel widget
-    newWindow.title(programName)
+#Make the frame for the buttons
+middleFrame = Frame(master)
+middleFrame.pack()
 
-    #Sets the Geometry of TopLevel
-    newWindow.geometry("200x200")
+#Launch Asset Manager
+assetManagerButton = Button(middleFrame, text = "Asset Manager")
+assetManagerButton.pack(side = TOP)
 
+#Input List Button
+inputListButton = Button(middleFrame, text = "Input List")
+inputListButton.pack()
+
+#Stage Plot Button
+stagePlotButton = Button(middleFrame, text = "Stage Plot")
+stagePlotButton.pack(side = BOTTOM)
 
 mainloop()
