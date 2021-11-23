58d7c3f92c5ad7ec908c75f65d9b161aceaf74e1
Trey Carey <treyacarey@icloud.com>
2021-11-23 00:23:35 -0600

    Changed window size, spacing and version label

--- Main.py
+++ Main.py
@@ -5,35 +5,42 @@ from tkinter import *
 from tkinter.ttk import *
 
 #Global Variables
-programName = "New Window"
+programName = "Technical Manager"
+versionNumber = "V0.01"
 
 #Creates the TK() Object
 master = Tk()
 
 #Sets the Geometry & Title of main root window
-master.geometry("1024x768")
+master.geometry("256x256")
 master.title(programName)
-
-welcomeLabel = Label(master, text = "Welcome to " + programName + "!")
-welcomeLabel.pack()
-
-optionsText = Label(master, text = "Please choose one of the following options")
-optionsText.pack()
 
 #Make the frame for the buttons
 middleFrame = Frame(master)
-middleFrame.pack()
+middleFrame.pack(fill = "both", expand = True, padx = 20, pady = 20)
+
+#Create Welcome Label
+welcomeLabel = Label(middleFrame, text = "Welcome to " + programName + "!")
+welcomeLabel.pack(side = TOP)
+
+#Create options text
+optionsText = Label(middleFrame, text = "Please choose one of the following options")
+optionsText.pack(side = TOP, pady = 20)
 
 #Launch Asset Manager
 assetManagerButton = Button(middleFrame, text = "Asset Manager")
-assetManagerButton.pack(side = TOP)
+assetManagerButton.pack(side = TOP, fill = "x")
 
 #Input List Button
 inputListButton = Button(middleFrame, text = "Input List")
-inputListButton.pack()
+inputListButton.pack(side = TOP, fill = "x")
 
 #Stage Plot Button
 stagePlotButton = Button(middleFrame, text = "Stage Plot")
-stagePlotButton.pack(side = BOTTOM)
+stagePlotButton.pack(side = TOP, fill = "x")
+
+#Create Versioning Label
+versionLabel = Label(middleFrame, text = versionNumber)
+versionLabel.pack(side = BOTTOM, pady = 10)
 
 mainloop()
