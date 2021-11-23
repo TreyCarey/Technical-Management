5b5d092feab990a77d659c29e868af242cb54762
Trey Carey <treyacarey@icloud.com>
2021-11-22 19:25:24 -0600

    Make renaming windows easier

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@
 from tkinter import *
 from tkinter.ttk import *
 
+#Global Variables
+programName = "New Window"
+
 #Creates the TK() Object
 master = Tk()
 
@@ -11,7 +14,7 @@ master = Tk()
 #Root window
 master.geometry("200x200")
 
-master.title("New Window")
+master.title(programName)
 
 #Function to open a new window
 #On a button click
@@ -21,7 +24,7 @@ def openNewWindow():
     newWindow = Toplevel(master)
 
     #Sets the title of the TopLevel widget
-    newWindow.title("New Window")
+    newWindow.title(programName)
 
     #Sets the Geometry of TopLevel
     newWindow.geometry("200x200")
